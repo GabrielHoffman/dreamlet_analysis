73f58bb7e285245ebf529f1407c5dc0c717f6bf8
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-09-06 13:37:24 -0400

    initial commit

---
+++ Nathan_NatImm_2021/get_data.R
@@ -0,0 +1,45 @@
+# Gabriel Hoffman
+# 
+# September 6, 2022
+#
+# Download and format data from Nathan, et al. 2021
+
+library(data.table)
+library(SingleCellExperiment)
+library(zellkonverter)
+library(stringr)
+source("read.sparseMatrix.R")
+
+# Download data from GEO
+#-----------------------
+cmd1 = "wget --no-check-certificate https://ftp.ncbi.nlm.nih.gov/geo/series/GSE158nnn/GSE158769/suppl/GSE158769%5Fmeta%5Fdata%2Etxt%2Egz"
+
+cmd2 = "wget --no-check-certificate https://ftp.ncbi.nlm.nih.gov/geo/series/GSE158nnn/GSE158769/suppl/GSE158769%5Fexprs%5Fraw%2Etsv%2Egz"
+
+system(cmd1)
+system(cmd2)
+
+# Read data
+#----------
+
+# read metadata
+df_meta = as.data.frame(fread('GSE158769_meta_data.txt.gz'))
+rownames(df_meta) = df_meta$cell_id
+
+# read counts as sparseMatrix
+counts = read.sparseMatrix("GSE158769_exprs_raw.tsv.gz", 100)
+
+# free memory
+gc() 
+
+# Convert to SingleCellExperiment and save as H5AD
+#-------------------------------------------------
+
+# create SingleCellExperiment
+sce = SingleCellExperiment( assays = list(counts = counts),
+                            colData = df_meta[colnames(counts),])
+
+# write to h5ad
+outpath = "/sc/arion/projects/CommonMind/hoffman/scRNAseq_data/Nathan_NatImm_2021/"
+outfile = paste0(outpath, '/Nathan_NatImm_2021.h5ad')
+writeH5AD(sce, outfile, compression="lzf", verbose=TRUE)
---
+++ Nathan_NatImm_2021/read.sparseMatrix.R
@@ -0,0 +1,49 @@
+
+library(dplyr)
+library(stringr)
+
+
+#' Read sparseMatrix efficiently
+#'
+#' Reading a big matrix requires too much memory. If matrix is sparse, read it line-by-line and convert to sparseMatrix. Assume first row and first column are labels
+#'
+#' @param file file path
+#' @param batchSize number of lines to process per batch 
+read.sparseMatrix = function(file, batchSize=100){
+
+  myCon = gzfile(file, open="r")
+
+  lst = list()
+  i = 1
+  repeat{
+
+    if( i == 1){
+      # read 1 line
+      pl = readLines(myCon, n = 1)
+      # if first line, set colnames
+      colNames = str_split(pl, "\t")[[1]][-1]
+    }else{
+      pl = readLines(myCon, n = batchSize)
+
+      # If the line is empty, exit.
+      if(identical(pl, character(0))){break} 
+
+      # else set values and rownames
+      res = str_split(pl, "\t")
+      mat = lapply(res, function(x){
+        values = as(as.integer(x[-1]), "sparseMatrix")
+        colnames(values) = x[1]
+        values
+        })
+      mat = t(do.call(cbind, mat))
+      lst[[i]] = mat
+      cat("\r", i*batchSize, "    ")
+    }
+    i = i + 1
+  }
+  close(myCon)
+
+  counts = bind_rows(lst)
+  colnames(counts) = colNames
+  counts
+}
